21c5aabfd606b19c22e9cc07d8ba770b5b0c922f
ShinDaHye <42795976+ShinDaHye@users.noreply.github.com>
2018-11-12 19:40:45 +0900

    saltt : db connect

--- app/src/main/java/com/example/dahye/wlb/MainActivity.java
+++ app/src/main/java/com/example/dahye/wlb/MainActivity.java
@@ -4,6 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
@@ -11,6 +14,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
     }
 
     @Override
